f5acf00f3f70a2e0a22765f7a2936a49a3d30158
Folkmann <theme1256@gmail.com>
2020-01-07 11:04:39 +0100

    Tilføjede Account og Player klasser fra sidste projekt

--- src/main/java/Model/Account.java
+++ src/main/java/Model/Account.java
@@ -1,4 +1,14 @@
 package Model;
 
 public class Account {
+    public int balance;
+
+    Account(int startBalance){
+        this.balance = startBalance;
+    }
+
+    public int updateBalance(int diff){
+        this.balance += diff;
+        return this.balance;
+    }
 }
