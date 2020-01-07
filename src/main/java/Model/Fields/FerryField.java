5e2662c8b2fc6ccc7775330fd81d82240d8a379a
soleimani1984 <z.soleimani1984@gmail.com>
2020-01-07 11:48:12 +0100

    FerryField class added and FerryField bord list added to BoardController

---
+++ src/main/java/Model/Fields/FerryField.java
@@ -0,0 +1,16 @@
+package Model.Fields;
+
+public class FerryField extends Field {
+    String name;
+    int pris;
+    int nr;
+
+    //Constructor
+    public FerryField(String name, int pris, int nr){
+        this.name = name;
+        this.pris = pris;
+        this.nr = nr;
+
+    }
+
+}
