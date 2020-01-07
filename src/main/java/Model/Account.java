6cbc945617ceb57983a422fb8f8677ca459323ef
soleimani1984 <z.soleimani1984@gmail.com>
2020-01-07 12:49:10 +0100

    some codes added to Account class

--- src/main/java/Model/Account.java
+++ src/main/java/Model/Account.java
@@ -1,4 +1,21 @@
 package Model;
 
 public class Account {
+    int startBalance;
+    int updateBalance;
+
+    //Constructor
+    public Account (int startBalance){
+        this.startBalance = startBalance;
+    }
+
+
+    // get and set methods
+    public int getUpdateBalance() {
+        return updateBalance;
+    }
+
+    public void setUpdateBalance(int updateBalance) {
+        this.updateBalance = updateBalance;
+    }
 }
