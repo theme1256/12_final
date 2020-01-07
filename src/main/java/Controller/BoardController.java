ea9d454f29c0fd51828263797660a6b9007d5f3a
Christine Nielsen <christine.nielsen93@gmail.com>
2020-01-07 13:29:57 +0100

    Indskrev antal spillere og startbalance i boardcontroller

--- src/main/java/Controller/BoardController.java
+++ src/main/java/Controller/BoardController.java
@@ -7,7 +7,32 @@ import gui_fields.GUI_Street;
 import gui_main.GUI;
 
 public class BoardController {
+    private static int numberOfPlayers = 0;
+    private static GUI gui;
+    private static int startBalance = 0;
+    private static Player[] players;
 
     public BoardController() {
     }
+g
+private static void getNumberOfPlayers() {
+        System.out.println("Indtast ønskede antal spillere");
+
+        numberOfPlayers = gui.getUserInteger("Indtast ønskede antal spillere", 3, 6);
+}
+
+private static void getNumberOfPlayers() {
+        getNumberOfPlayers();
+        players =  Player[numberOfPlayers];
+
+        if ((numberOfPlayers >=3) && (numberOfPlayers <= 6))
+            startBalance = 1500;
+        else        {
+            System.out.println("Dette antal spillere er ikke understøttet");
+            gui.showMessage("Dette antal spillere er ikke understøttet");
+        }
+}
+
+
+
 }
