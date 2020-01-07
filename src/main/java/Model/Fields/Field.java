58e45b389cad197a138c2f9ece43afcc8ba6145f
soleimani1984 <z.soleimani1984@gmail.com>
2020-01-07 14:03:37 +0100

    Using Field as a supper class

--- src/main/java/Model/Fields/Field.java
+++ src/main/java/Model/Fields/Field.java
@@ -1,4 +1,15 @@
 package Model.Fields;
 
 public class Field {
+    int nr;
+    String description;
+
+    //Constructor
+    public Field(int nr, String description){
+        this.nr = nr;
+        this.description = description;
+    }
+
 }
+
+
