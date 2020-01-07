01a6d2ca4e7c78c0fc6c1030f703dd965d5c5356
soleimani1984 <z.soleimani1984@gmail.com>
2020-01-07 11:14:20 +0100

    TaxField class added

--- src/main/java/Controller/BoardController.java
+++ src/main/java/Controller/BoardController.java
@@ -1,13 +1,10 @@
 package Controller;
 
-import Model.*;
+import Model.ChanceCards.Card;
 import Model.Fields.Field;
 import Model.Fields.StartField;
 import Model.Fields.StreetField;
-import gui_fields.GUI_Field;
-import gui_fields.GUI_Player;
-import gui_fields.GUI_Street;
-import gui_main.GUI;
+import Model.Fields.TaxField;
 
 public class BoardController {
     private Field[] boardList = new Field[40];
@@ -24,12 +21,12 @@ public class BoardController {
     public BoardController() {
         boardList[0] = new StartField();
         boardList[1] = new StreetField(60, 2, "Rødovrevej", "blue");
-        boardList[2] = new ???;
+        //boardList[2] = new Card();
         boardList[3] = new StreetField(60, 4, "Hvidovre", "blue");
-        boardList[4] = new ;
+        boardList[4] = new TaxField("Indkomstskat", 200);
         boardList[5] = new ;
         boardList[6] = new StreetField(100,7,"Roskildevej", "pink");
-        boardList[7] = new ???;
+        //boardList[7] = new Card();
         boardList[8] = new StreetField(100, 9, "Valby Langgade", "pink");
         boardList[9] = new StreetField(120, 10, "Allégade","pink");
         boardList[10] = new ;
@@ -39,12 +36,12 @@ public class BoardController {
         boardList[14] = new StreetField(160,15,"Gl. Kongevej","green");
         boardList[15] = new ;
         boardList[16] = new StreetField(180,17,"Bernstorffsvej","grey");
-        boardList[17] = new ???;
+        //boardList[17] = new Card();
         boardList[18] = new StreetField(180,19,"Hellerupvej","grey");
         boardList[19] = new StreetField(200,20,"Strandvej","grey");
         boardList[20] = new ;
         boardList[21] = new StreetField(220,22,"Trianglen","red");
-        boardList[22] = new ???;
+        //boardList[22] = new Card();
         boardList[23] = new StreetField(220,24,"Østrebrogade","red");
         boardList[24] = new StreetField(240,25,"Grønningen","red");
         boardList[25] = new ;
@@ -55,12 +52,12 @@ public class BoardController {
         boardList[30] = new ;
         boardList[31] = new StreetField(300,32,"Amagertorv","yellow");
         boardList[32] = new StreetField(300,33,"Vimmelskaftet","yellow");
-        boardList[33] = new ???;
+        //boardList[33] = new Card();
         boardList[34] = new StreetField(320,35,"Nygade","yellow");
         boardList[35] = new ;
-        boardList[36] = new ???;
+        //boardList[36] = new Card();
         boardList[37] = new StreetField(350,38,"Frederiksborggade","dark red");
-        boardList[38] = new ;
+        boardList[38] = new TaxField("Ekstraordinær Statsskat", 100);
         boardList[39] = new StreetField(400,40,"Rådhuspladsen","dark red");
 
 
