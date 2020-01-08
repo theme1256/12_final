b327f91c7eda9092ef5e939fab6ca0415bd82df9
Christian Pone <ChristiaNotarmaso@gmail.com>
2020-01-08 15:44:36 +0100

    Små rettelser

---
+++ src/main/java/Model/Fields/TaxField.java
@@ -0,0 +1,8 @@
+package Model.Fields;
+
+public class TaxField extends Field {
+
+    public TaxField(String name, String description, int price, int nr) {
+        super(name, description, price, nr);
+    }
+}
