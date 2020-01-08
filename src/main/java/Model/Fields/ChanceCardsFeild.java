63f48fc901882a67348ac9def6cced1cbe7ea02b
soleimani1984 <z.soleimani1984@gmail.com>
2020-01-08 11:44:07 +0100

    small changes

--- src/main/java/Model/Fields/ChanceCardsFeild.java
+++ src/main/java/Model/Fields/ChanceCardsFeild.java
@@ -2,7 +2,7 @@ package Model.Fields;
 
 public class ChanceCardsFeild extends Field {
 
-    public ChanceCardsFeild(String name, String description, int nr) {
+    public ChanceCardsFeild(String name, String description, int price, int nr) {
         super(name, description, price, nr);
     }
 }
