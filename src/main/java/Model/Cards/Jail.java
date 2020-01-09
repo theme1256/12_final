68c8d1e3c1bed693636b8c190499711ac188dc9c
Amer Biro <amer.biro@gmail.com>
2020-01-09 14:22:05 +0100

    Action methods have been added to Jail card class and should be tested.

--- src/main/java/Model/Cards/Jail.java
+++ src/main/java/Model/Cards/Jail.java
@@ -1,10 +1,23 @@
 package Model.Cards;
 
+import Model.Player;
+import gui_main.GUI;
+
 public class Jail extends Kort {
     public Jail(int cardNumber) {
         super(cardNumber);
     }
+
+    @Override
+    public void action(Player player, GUI gui) {
+        player.setJailPass(true);
+        gui.displayChanceCard(toString());
+    }
+
     public String toString(){
+        cardDescription = "";
+        action = "";
+        value = 0;
         switch (cardNumber){
             case 0:
                 cardDescription += "I anledning af Kongens fødselsdag benådes De herved for fængsel.\n" +
