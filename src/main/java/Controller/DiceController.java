d8e6e15f93697b41fdbb9a0e5d9911d16ca02279
Christian Pone <ChristiaNotarmaso@gmail.com>
2020-01-10 12:35:19 +0100

    Tilføjet flere controllers

--- src/main/java/Controller/DiceController.java
+++ src/main/java/Controller/DiceController.java
@@ -1,4 +1,59 @@
 package Controller;
+import Model.Player;
+
 
 public class DiceController {
+
+    static boolean handleRound(Player player) {
+
+        // Slå med terningen når spilleren trykker
+        BoardControllerGUI.gui.getUserButtonPressed(player + ", tryk enter/knappen for at slå", "SLÅ");
+
+        // Vis resultatet og opdater felt
+        int[] val = GameController.shaker.shake();
+        BoardControllerGUI.gui.setDice(val[0],val[1]);
+        int value = val[0] + val[1];
+
+        player.move(value);
+
+        PlayerController.handleGetInJail(player);
+        //Håndterer chancekort
+        ChanceCardController.handleChancekort(player);
+
+        if(val[0]==val[1]){
+
+            BoardControllerGUI.gui.getUserButtonPressed(player + ", får en ekstra tur!", "FEDT!");
+            BoardControllerGUI.gui.getUserButtonPressed(player + ", tryk enter/knappen for at slå", "SLÅ");
+            int[] val1 = GameController.shaker.shake();
+            BoardControllerGUI.gui.setDice(val1[0],val1[1]);
+            int value1 = val1[0] + val1[1];
+            player.move(value1);
+            PlayerController.handleGetInJail(player);
+
+            if(val1[0]== val1[1] ){
+                BoardControllerGUI.gui.getUserButtonPressed(player + ", får en ekstra tur!", "FEDT!");
+                BoardControllerGUI.gui.getUserButtonPressed(player + ", tryk enter/knappen for at slå", "SLÅ");
+
+                System.out.println("Tillykke du får en ekstra tur");
+                int[] val2 = GameController.shaker.shake();
+                BoardControllerGUI.gui.setDice(val2[0],val2[1]);
+                int value2 = val2[0] + val2[1];
+                player.move(value2);
+                PlayerController.handleGetInJail(player);
+
+                if(val2[0]== val2[1]){
+                    BoardControllerGUI.gui.getUserButtonPressed(player + ", er for heldig til det kan være rigtigt! Du ryger i fængsel", "ØV!");
+                    player.moveTo(10,false);
+
+                }
+            }
+        }
+
+        //Hvis en spiller lander på et felt over felt 39; så starterde forfra på brættet og chekcer om spilleren skal have 200kr.
+        PlayerController.handlePassStart(player);
+
+        return  player.account.balance > 0;
+    }
+
+
 }
--- src/main/java/Controller/GameController.java
+++ src/main/java/Controller/GameController.java
@@ -1,4 +1,60 @@
 package Controller;
 
+import Model.ChanceDeck;
+import Model.Player;
+import Model.Shaker;
+
 public class GameController {
+    static int numberOfPlayers = 0;
+    static int startBalance = 0;
+    static Shaker shaker;
+    static ChanceDeck chanceDeck;
+    static Player[] players;
+
+    public GameController(){
+        boolean playing = true;
+        while (playing) {
+            for (Player player : GameController.players) {
+                playing = DiceController.handleRound(player);
+                if(!playing)
+                    break;
+            }
+        }
+    }
+
+    static void initVars(){
+        shaker = new Shaker(2);
+        chanceDeck = new ChanceDeck();
+        chanceDeck.blandkort();
+        while (GameController.startBalance == 0) {
+            setStartBalance();
+        }
+
+        for (int i = 0; i < GameController.numberOfPlayers; i++) {
+            players[i] = new Player(BoardControllerGUI.gui, GameController.startBalance, i);
+
+            BoardControllerGUI.gui.showMessage("Navn: " + players[i].playerName + "\nStart-balance: " + players[i].account.balance);
+        }
+    }
+
+    private static void getNumberOfPlayers() {
+        System.out.println("Indtast ønskede antal spillere");
+        numberOfPlayers = BoardControllerGUI.gui.getUserInteger("Indtast ønskede antal spillere", 3, 6);
+    }
+
+    static void setStartBalance() {
+        getNumberOfPlayers();
+        GameController.players = new Player[numberOfPlayers];
+
+        if ((numberOfPlayers >= 3) && (numberOfPlayers <= 6))
+            startBalance = 1500;
+        else {
+            System.out.println("Dette antal spillere er ikke understøttet");
+            BoardControllerGUI.gui.showMessage("Dette antal spillere er ikke understøttet");
+        }
+    }
+
+
+
+
 }
