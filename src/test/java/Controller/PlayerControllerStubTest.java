29f05c354f8af081ae7b99808103f4c204dbbaa0
Christian Pone <ChristiaNotarmaso@gmail.com>
2020-01-14 16:10:42 +0100

    Tilføjet tests

--- src/test/java/Controller/PlayerControllerStubTest.java
+++ src/test/java/Controller/PlayerControllerStubTest.java
@@ -9,14 +9,15 @@ import static org.junit.Assert.*;
 
 public class PlayerControllerStubTest {
 
-    PlayerStub[] players;
+    private PlayerStub[] players;
+    private PlayerStub player = new PlayerStub(0,"Henrik");
     AccountStub accountStub;
 
-    DiceControllerStub dc = new DiceControllerStub();
-    PlayerControllerStub pc = new PlayerControllerStub(dc);
+    private DiceControllerStub dc = new DiceControllerStub();
+    private PlayerControllerStub pc = new PlayerControllerStub(dc);
 
-    int startBalance = 3;
-    int numberOfPlayers = 3;
+    private int startBalance = 3;
+    private int numberOfPlayers = 3;
 
     @Test
     public void createPlayers() {
@@ -33,10 +34,47 @@ public class PlayerControllerStubTest {
 
     @Test
     public void handlePassStart() {
+        player.passedStart = false;
+        player.moveTo(2,true);
+        assertEquals(0,player.getBalance());
+        if(player.passedStart){
+            player.updateBalance(200);
+            assertEquals(200,player.getBalance());
+            player.passedStart = false;
+            assertTrue(false);
+
+        }
+
     }
 
     @Test
     public void handleGetInJail() {
+        System.out.println("Balance = "+ player.getBalance());
+        player.moveTo(39);
+        player.moveTo(35, true);
+        if(player.currentFelt == 30){
+            player.moveTo(10,false);
+        }else {System.out.println("Du ryger ikke i fængsel og modtager 200kr");
+            player.updateBalance(200);}
+
+        System.out.println("Balance = "+ player.getBalance());
+        assertFalse(player.getIsInJail());
+        assertTrue(player.passedStart);
+
+
+        player.moveTo(30,true);
+        if(player.currentFelt == 30){
+            System.out.println("Du ryger i fængsel og modtager ikke 200kr");
+            player.moveTo(10,false);
+            player.setIsInJail(true);
+
+        }else {System.out.println("Du ryger ikke i fængsel");
+        player.updateBalance(200);}
+
+        System.out.println("Balance = "+ player.getBalance());
+        assertTrue(player.getIsInJail());
+        assertEquals(200,player.getBalance());
+
     }
 
     @Test
