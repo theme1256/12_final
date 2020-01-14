2f91d255343615eca0d547a33912ff02fbd17f3b
Folkmann <theme1256@gmail.com>
2020-01-14 11:27:19 +0100

    Lidt arbejde på "Giv op" og kan nu finde ud af hvem der vandt

--- src/main/java/Model/Fields/Property.java
+++ src/main/java/Model/Fields/Property.java
@@ -1,6 +1,9 @@
 package Model.Fields;
 
 import Model.Player;
+import gui_fields.GUI_Field;
+import gui_fields.GUI_Ownable;
+import gui_fields.GUI_Shipping;
 import gui_main.GUI;
 
 import java.awt.*;
@@ -19,6 +22,17 @@ public abstract class Property extends Field {
     }
 
 
+    /**
+     * Henter den version af feltet, som GUI bruger
+     *
+     * @param gui Pointer til det aktive GUI
+     * @return Et felt, som der kan bruges til at modificere GUI
+     */
+    private GUI_Ownable getGuiVersion(GUI gui) {
+        GUI_Field[] fields = gui.getFields();
+        return ((GUI_Ownable) fields[nr - 1]);
+    }
+
     protected abstract int calculateRent(Field[] felter);
     public abstract void action(GUI gui, Player player);
     public abstract void action(GUI gui, Player player, Field[] felter);
@@ -29,7 +43,16 @@ public abstract class Property extends Field {
         this.owner = player;
         this.owned = true;
     }
-    public void sell() {
+    public boolean isOwned() {
+        return owned;
+    }
+    public void sell(GUI gui) {
+        owner.updateBalance(this.price);
+        if (gui != null) {
+            GUI_Ownable GUIv = getGuiVersion(gui);
+            GUIv.setRent("");
+            GUIv.setOwnerName("");
+        }
         this.owner = null;
         this.owned = false;
     }
