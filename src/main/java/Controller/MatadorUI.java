0f0b1d1cca867dfceb722f1490e7d9c64eb29fd6
Folkmann <theme1256@gmail.com>
2020-01-16 11:58:27 +0100

    Eksempel på hvordan man laver kode til faste slag til testcases

--- src/main/java/Controller/MatadorUI.java
+++ src/main/java/Controller/MatadorUI.java
@@ -17,6 +17,59 @@ public class MatadorUI {
         ChanceCardController chanceCardController = new ChanceCardController(fieldController, gui);
 
         DiceController diceController = new DiceController(gui);
+        if (args.length > 0) {
+            switch (args[0]) {
+                case "TC2":
+                    diceController.overrideShakes(new int[][]{
+                            {1, 2},
+                            {1, 2},
+                            {1, 2},
+                    });
+                    break;
+                case "TC3":
+                    diceController.overrideShakes(new int[][]{
+                            {1, 3},
+                            {1, 3},
+                            {1, 3},
+                    });
+                    break;
+                case "TC4":
+                    diceController.overrideShakes(new int[][]{
+                            {1, 4},
+                            {1, 4},
+                            {1, 4},
+                    });
+                    chanceCardController = new ChanceCardController(fieldController, gui, true);
+                    break;
+                case "TC5":
+                    diceController.overrideShakes(new int[][]{
+                            {1, 5},
+                            {1, 5},
+                            {1, 5},
+                    });
+                    chanceCardController = new ChanceCardController(fieldController, gui, true);
+                    break;
+                case "TC6":
+                    diceController.overrideShakes(new int[][]{
+                            {1, 6},
+                            {1, 6},
+                            {1, 6},
+                    });
+                    chanceCardController = new ChanceCardController(fieldController, gui, true);
+                    break;
+                case "TC7":
+                    diceController.overrideShakes(new int[][]{
+                            {2, 6},
+                            {2, 6},
+                            {2, 6},
+                    });
+                    chanceCardController = new ChanceCardController(fieldController, gui, true);
+                    break;
+                default:
+                    System.out.println("Den testcase blev ikke genkendt, fik: " + args[0]);
+                    break;
+            }
+        }
 
         PlayerController playerController = new PlayerController(gui, diceController);
 
