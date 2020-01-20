77afaeec9cdc88d5fe281672f8b50f8cce154734
Folkmann <theme1256@gmail.com>
2020-01-20 08:49:35 +0100

    Dokumentation

--- src/main/java/Model/ChanceDeck.java
+++ src/main/java/Model/ChanceDeck.java
@@ -57,14 +57,13 @@ public class ChanceDeck {
      */
     public ChanceCards draw() {
         // Tager en kopi af det første kort i array'et
-        ChanceCards trukket = cards[0];
+        ChanceCards trukket = this.cards[0];
         // Løber kortene igennem og flytter dem med en
-        for (int i = 0; i < cards.length - 1; i++) {
-            cards[i] = cards[i + 1];
-
+        for (int i = 0; i < this.cards.length - 1; i++) {
+            this.cards[i] = this.cards[i + 1];
         }
         // Tilføjer det først kort til enden
-        cards[cards.length - 1] = trukket;
+        this.cards[this.cards.length - 1] = trukket;
         // Returnerer det første kort
         return trukket;
     }
@@ -94,19 +93,29 @@ public class ChanceDeck {
      * @param newOrder Et array af numre, i den rækkefølge de skal trækkes
      */
     public void setOrder(int[] newOrder) {
-        ChanceCards[] tmp = new ChanceCards[32];
+        // Opretter et midlertidigt array til at holde den nye rækkefølge på kortene
+        ChanceCards[] tmp = new ChanceCards[this.cards.length];
+
+        // For alle de tal der er givet i "newOrder", kopier fra den usorterede liste
         for (int i = 0; i < newOrder.length; i++) {
             tmp[i] = this.cards[newOrder[i]];
         }
-        if (newOrder.length < cards.length) {
+
+        // Hvis der ikke er givet en ny rækkefølge, som er lige så lang som det originale dæk
+        if (newOrder.length < this.cards.length) {
+            // Den placering, som der skal sættes ind ved, da der allerede er kopieret nogen elementer
             int i = newOrder.length;
+            // Løb alle kortene igennem
             for (ChanceCards card : this.cards) {
+                // Hvis den nye liste ikke indeholder det nuværrende kort, tilføj det og til index 1 op
                 if (!this.contains(tmp, card)) {
                     tmp[i] = card;
                     i++;
                 }
             }
         }
+
+        // Overskiv listen med kort, men den midlertidige der lige er oprettet
         this.cards = tmp;
     }
 
@@ -118,10 +127,15 @@ public class ChanceDeck {
      * @return Om kortet blev fundet
      */
     public boolean contains(ChanceCards[] array, ChanceCards v) {
+        // Opretter en variabel der kan returneres, som som standard er falsk
         boolean result = false;
+        // Hvis der er givet to variabeler
         if (array != null && v != null) {
+            // For alle de chancekort, som "array" gerne skulle bestå af
             for (ChanceCards i : array) {
+                // Hvis det er et chancekort
                 if (i != null) {
+                    // Hvis de to chancekort's numre er det samme, overskriv "restult" og stop loop
                     if (i.getCardNumber() == v.getCardNumber()) {
                         result = true;
                         break;
@@ -132,7 +146,8 @@ public class ChanceDeck {
         return result;
     }
 
+    @Override
     public String toString() {
-        return Arrays.asList(cards).toString();
+        return Arrays.asList(this.cards).toString();
     }
 }
